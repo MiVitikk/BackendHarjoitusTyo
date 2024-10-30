884ca7336531134d1170f3425086b502d6c7884d
Hilla Nokela <mikko.vitikka@myy.haaga-helia.fi>
2024-10-30 14:02:50 +0200

    Deploy commit 7

--- src/main/java/harjoitustyo/harjoitustyo/WebSecurityConfig.java
+++ src/main/java/harjoitustyo/harjoitustyo/WebSecurityConfig.java
@@ -31,49 +31,51 @@ import java.util.List;
 @EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
 
-    @Autowired
-    private UserDetailServiceImpl userDetailsService;
-
-    private static final AntPathRequestMatcher[] WHITE_LIST_URLS = {
-            new AntPathRequestMatcher("/api/players**"),
-            new AntPathRequestMatcher("/h2-console/**"),
-            new AntPathRequestMatcher("/api/**") };
-
-    @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        
-                        .requestMatchers(antMatcher("/css/**")).permitAll()
-                        
-                        .requestMatchers(WHITE_LIST_URLS).permitAll()
-                        .anyRequest().authenticated())
-                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions
-                        .disable()))
-                .formLogin(formlogin -> formlogin
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/index", true)
-                        .permitAll())
-                .logout(logout -> logout
-                        .permitAll())
-                .csrf(csrf -> csrf.disable());
-        return http.build();
-    }
-
+        private final UserDetailServiceImpl userDetailsService;
+        private final PasswordEncoder passwordEncoder;
     
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(new BCryptPasswordEncoder()); 
-        return authProvider;
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
-    }
+        // Constructor Injection
+        @Autowired
+        public WebSecurityConfig(UserDetailServiceImpl userDetailsService, PasswordEncoder passwordEncoder) {
+            this.userDetailsService = userDetailsService;
+            this.passwordEncoder = passwordEncoder;
+        }
+    
+        private static final AntPathRequestMatcher[] WHITE_LIST_URLS = {
+                new AntPathRequestMatcher("/api/players**"),
+                new AntPathRequestMatcher("/h2-console/**"),
+                new AntPathRequestMatcher("/api/**") 
+        };
+    
+        @Bean
+        public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeHttpRequests(authorize -> authorize
+                            .requestMatchers(antMatcher("/css/**")).permitAll()
+                            .requestMatchers(WHITE_LIST_URLS).permitAll()
+                            .anyRequest().authenticated())
+                    .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
+                    .formLogin(formlogin -> formlogin
+                            .loginPage("/login")
+                            .defaultSuccessUrl("/index", true)
+                            .permitAll())
+                    .logout(logout -> logout.permitAll())
+                    .csrf(csrf -> csrf.disable());
+            return http.build();
+        }
+    
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+    
+        @Bean
+        public DaoAuthenticationProvider authenticationProvider() {
+            DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+            authProvider.setUserDetailsService(userDetailsService);
+            authProvider.setPasswordEncoder(passwordEncoder);
+            return authProvider;
+        }
 
     
 }
